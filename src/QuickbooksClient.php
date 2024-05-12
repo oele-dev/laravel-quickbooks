de867f7d220d79bdc41a859d74bcd6705e8fe5d4
Osmell Caicedo <correo.oele@gmail.com>
2024-05-12 15:32:45 -0500

    fix typo and load redirect url from configuration

--- src/QuickbooksClient.php
+++ src/QuickbooksClient.php
@@ -14,25 +14,20 @@ class QuickbooksClient
 {
     /**
      * Return authentication url
-     *
-     * @return string
      */
     public function getAuthorizationUrl(): string
     {
-        return $this->confgiureDataService()
+        return $this->configureDataService()
             ->getOAuth2LoginHelper()
             ->getAuthorizationCodeURL();
     }
 
     /**
      * Create new token
-     *
-     * @param  Request  $request
-     * @return void
      */
     public function createToken(Request $request): void
     {
-        $token = $this->confgiureDataService()
+        $token = $this->configureDataService()
             ->getOAuth2LoginHelper()
             ->exchangeAuthorizationCodeForToken(
                 $request->input('code'),
@@ -50,20 +45,17 @@ class QuickbooksClient
 
     /**
      * Get data service and refresh token
-     *
-     * @param Model $model
-     * @return DataService
      */
     public function getDataService(Model $model): DataService
     {
-        if (!$model->quickbooksToken) {
+        if (! $model->quickbooksToken) {
             throw new \Error('Unable to configure QuickBooks Data Service. No token found.');
         }
 
-        $dataService = $this->confgiureDataService([
-            'accessTokenKey'  => $model->quickbooksToken->access_token,
+        $dataService = $this->configureDataService([
+            'accessTokenKey' => $model->quickbooksToken->access_token,
             'refreshTokenKey' => $model->quickbooksToken->refresh_token,
-            'QBORealmID'      => $model->quickbooksToken->realm_id,
+            'QBORealmID' => $model->quickbooksToken->realm_id,
         ]);
 
         if (config('laravel-quickbooks.logging.enabled')) {
@@ -81,39 +73,33 @@ class QuickbooksClient
 
     /**
      * Return data service provider
-     *
-     * @param  array  $args
-     * @return DataService
      */
-    public function confgiureDataService(array $args = []): DataService
+    public function configureDataService(array $args = []): DataService
     {
         return DataService::Configure([
-            'auth_mode'    => 'oauth2',
-            'scope'        => 'com.intuit.quickbooks.accounting',
-            'ClientID'     => config('laravel-quickbooks.connection.client_id'),
+            'auth_mode' => 'oauth2',
+            'scope' => 'com.intuit.quickbooks.accounting',
+            'ClientID' => config('laravel-quickbooks.connection.client_id'),
             'ClientSecret' => config('laravel-quickbooks.connection.client_secret'),
-            'baseUrl'      => config('laravel-quickbooks.connection.base_url'),
-            'RedirectURI'  => config('app.url').'quickbooks/token',
+            'baseUrl' => config('laravel-quickbooks.connection.base_url'),
+            'RedirectURI' => config('laravel-quickbooks.connection.redirect_url'),
             ...$args,
         ]);
     }
 
     /**
      * Parse token
-     *
-     * @param  OAuth2AccessToken  $token
-     * @return array
      */
     public function parseToken(OAuth2AccessToken $token): array
     {
-        $accessExpiry  = (string) $token->getAccessTokenExpiresAt();
+        $accessExpiry = (string) $token->getAccessTokenExpiresAt();
         $refreshExpiry = (string) $token->getRefreshTokenExpiresAt();
 
         return [
-            'realm_id'                 => $token->getRealmID(),
-            'access_token'             => $token->getAccessToken(),
-            'refresh_token'            => $token->getRefreshToken(),
-            'access_token_expires_at'  => Carbon::parse($accessExpiry),
+            'realm_id' => $token->getRealmID(),
+            'access_token' => $token->getAccessToken(),
+            'refresh_token' => $token->getRefreshToken(),
+            'access_token_expires_at' => Carbon::parse($accessExpiry),
             'refresh_token_expires_at' => Carbon::parse($refreshExpiry),
         ];
     }
